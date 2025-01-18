5130287dfcb590b88094d1232b64ef62d27ce938
okita <okitasougo0413@yahoo.co.jp>
2025-01-18 15:14:50 +0900

    test2回目

--- 20250118/okita.php
+++ 20250118/okita.php
@@ -1,1 +1,11 @@
-こんにちは
+こんにちは!
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>2</h1>
+</body>回目
+</html>
